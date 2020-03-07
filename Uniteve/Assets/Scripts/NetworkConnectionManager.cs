5f40e69c83e9b304c2bb6de407692dd660c763ac
Rasengangstarr <davidouthere@msn.com>
2020-03-07 18:43:19 +0000

    got basic photon room logic working

---
+++ Uniteve/Assets/Scripts/NetworkConnectionManager.cs
@@ -0,0 +1,32 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Uniteve {
+
+    public class NetworkConnectionManager : MonoBehaviourPunCallbacks
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            PhotonNetwork.OfflineMode = false;
+            PhotonNetwork.NickName = "Player "+Random.value;
+            PhotonNetwork.AutomaticallySyncScene = true;
+            PhotonNetwork.GameVersion = "v1";
+
+            PhotonNetwork.ConnectUsingSettings();
+
+            
+        }
+
+        public override void OnConnectedToMaster() {
+            PhotonNetwork.CreateRoom("Test Room!");
+        }
+
+        public override void OnJoinedRoom()
+        {
+            base.OnJoinedRoom();
+            Debug.Log("Master: " + PhotonNetwork.IsMasterClient);
+        }
+    }
+}
